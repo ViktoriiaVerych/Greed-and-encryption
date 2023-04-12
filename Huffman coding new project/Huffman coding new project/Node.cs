77041a3c8bbb6b6168d233289e576dd099b2d8ad
annagorbatenko <ahorbatenko@kse.org.ua>
2023-04-12 17:10:18 +0300

    not finished node

--- Huffman coding new project/Huffman coding new project/Node.cs
+++ Huffman coding new project/Huffman coding new project/Node.cs
@@ -1,6 +1,53 @@
-﻿namespace Huffman_coding_new_project;
+﻿using System.Collections.Generic;
 
-public class Node
+namespace Huffman_coding_new_project
 {
-    
+    public class Node
+    {
+        public char Symbol { get; set; }
+        public int Frequency { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+
+        public List<bool> Traverse(char symbol, List<bool> data = null)
+        {
+            if (data == null)
+                data = new List<bool>();
+
+            // Leaf
+            if (Right == null && Left == null)
+            {
+                if (symbol.Equals(this.Symbol))
+                    return data;
+                else
+                    return null;
+            }
+            else
+            {
+                List<bool> left = null;
+                List<bool> right = null;
+
+                if (Left != null)
+                {
+                    List<bool> leftPath = new List<bool>();
+                    leftPath.AddRange(data);
+                    leftPath.Add(false);
+                    left = Left.Traverse(symbol, leftPath);
+                }
+
+                if (Right != null)
+                {
+                    List<bool> rightPath = new List<bool>();
+                    rightPath.AddRange(data);
+                    rightPath.Add(true);
+                    right = Right.Traverse(symbol, rightPath);
+                }
+
+                if (left != null)
+                    return left;
+                else
+                    return right;
+            }
+        }
+    }
 }

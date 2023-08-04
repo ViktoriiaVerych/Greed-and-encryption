75d3b611d9385b13989114ef56f0c0480f6689be
ViktoriiaVerych <113999567+ViktoriiaVerych@users.noreply.github.com>
2023-08-04 23:58:30 +0300

    Update Decoder.cs

--- Huffman coding new project/Huffman coding new project/Decoder.cs
+++ Huffman coding new project/Huffman coding new project/Decoder.cs
@@ -1,6 +1,6 @@
 ﻿namespace Huffman_coding_new_project;
 
-//
+/*
 1. The `Decoder` class has a method called `DecodeFile` which takes two inputs: a `Node` representing the root of a Huffman tree, and a list of strings named `codedText` which contains the encoded text.
 
 2. Inside the `DecodeFile` method:
@@ -17,7 +17,7 @@
 3. Once the loop completes, the method returns the `decodedFile`, which now contains the decoded text extracted from the input coded text using the provided Huffman tree structure.
 
 In essence, the code simulates traversing a Huffman tree based on the encoded bits, and at each leaf node, it collects the corresponding symbols to reconstruct the original text.
-//
+*/
 public class Decoder
 {
     public string DecodeFile(Node root, List<string>codedText)

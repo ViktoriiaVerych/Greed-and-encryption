8fb6a7e1b41ada24e4c8a7294b497e96add48ab8
ViktoriiaVerych <113999567+ViktoriiaVerych@users.noreply.github.com>
2023-08-04 23:57:52 +0300

    Update Decoder.cs

--- Huffman coding new project/Huffman coding new project/Decoder.cs
+++ Huffman coding new project/Huffman coding new project/Decoder.cs
@@ -1,5 +1,23 @@
 ﻿namespace Huffman_coding_new_project;
 
+//
+1. The `Decoder` class has a method called `DecodeFile` which takes two inputs: a `Node` representing the root of a Huffman tree, and a list of strings named `codedText` which contains the encoded text.
+
+2. Inside the `DecodeFile` method:
+   - It initializes a variable `decodedFile` to store the decoded text.
+   - It starts at the root of the Huffman tree (`current = root`).
+   - It combines all the strings in the `codedText` list to create a continuous string named `symbol`.
+   - It then iterates through each character (bit) in the `symbol` string.
+   - For each bit:
+     - If the bit is '0', it moves `current` to its left child in the Huffman tree.
+     - If the bit is '1', it moves `current` to its right child.
+     - If `current` becomes a leaf node (no left or right children), it appends the corresponding symbol to the `decodedFile` and resets `current` to the root.
+   - This process continues until all bits have been processed.
+
+3. Once the loop completes, the method returns the `decodedFile`, which now contains the decoded text extracted from the input coded text using the provided Huffman tree structure.
+
+In essence, the code simulates traversing a Huffman tree based on the encoded bits, and at each leaf node, it collects the corresponding symbols to reconstruct the original text.
+//
 public class Decoder
 {
     public string DecodeFile(Node root, List<string>codedText)
@@ -8,7 +26,7 @@ public class Decoder
         var current = root;
         var symbol = string.Join("", codedText);
         
-        //Траверс на основі бітів в закодованому тексті
+        
         foreach (var bit in symbol)
         {
         
@@ -21,7 +39,7 @@ public class Decoder
             {
                 current = current.Right;
             }
-            //досягли кінця, додаємо відповідний символ і повертаємось до root
+            
             if (current.Right == null && current.Left == null)
             {
                 decodedFile += current.Value;
